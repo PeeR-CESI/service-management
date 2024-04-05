d241d7a60384723a54dbd5cf5506cdaec9dc9339
Antoine Spiers <antoine.spiers@gmail.com>
2024-04-05 10:37:18 +0200

    feat: add swagger api implementation to code

--- src/app.py
+++ src/app.py
@@ -1,12 +1,31 @@
 from flask import Flask
+from flasgger import Swagger
 from services.routes import service_bp
 from avis.routes import avis_bp
 from helloWorld.routes import helloWorld_bp
 
 app = Flask(__name__)
+
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": 'apispec_1',
+            "route": '/apispec_1.json',
+            "rule_filter": lambda rule: True,  # Toutes les règles correspondent
+            "model_filter": lambda tag: True,  # Tous les modèles
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/apidocs/"
+}
+
+swagger = Swagger(app, config=swagger_config)
+
 app.register_blueprint(helloWorld_bp)
 app.register_blueprint(service_bp, url_prefix='/service')
 app.register_blueprint(avis_bp, url_prefix='/avis')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=500)
