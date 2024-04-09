1d0ca86490c1b3607b8bdb6cc88b5aefad3a1ca4
Antoine Spiers <antoine.spiers@gmail.com>
2024-04-10 01:58:28 +0200

    revert: bad syntax for service_id

--- src/services/service.py
+++ src/services/service.py
@@ -29,7 +29,5 @@ def get_all_services():
     services = Service.find_all()
     for service in services:
         # Convertir ObjectId en str
-        service_id = service.get('_id')  # Utilisez .get() pour éviter KeyError
-        if service_id:
-            service['service_id'] = str(service_id)
+        service['_id'] = str(service['_id'])
     return jsonify(services), 200
