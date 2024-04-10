c39b6c3cf333aa7b31bf11b00dd1e6846d24568b
Antoine Spiers <antoine.spiers@gmail.com>
2024-04-10 20:04:13 +0200

    revert: cors

--- src/services/service.py
+++ src/services/service.py
@@ -12,29 +12,18 @@ def update_service(service_id, service_data):
         return jsonify({"error": "Service not found"}), 404
 
 def delete_service(service_id):
-    service = Service.find(service_id)
-    if service:
-        presta_id = service.presta_id
-        success = Service.delete(service_id)
-        if success:
-            presta = User.query.get(presta_id)
-            if presta and presta.service_ids:
-                service_ids_list = presta.service_ids.split(',')
-                if str(service_id) in service_ids_list:
-                    service_ids_list.remove(str(service_id))
-                    presta.service_ids = ','.join(service_ids_list)
-                    db.session.commit()
-            return jsonify({"message": "Service deleted successfully"}), 200
-        else:
-            return jsonify({"error": "Service not found"}), 404
+    if Service.delete(service_id):
+        return jsonify({"message": "Service deleted successfully"}), 200
     else:
         return jsonify({"error": "Service not found"}), 404
+
 def get_service(service_id):
     service = Service.find(service_id)
     if service:
         return jsonify(service), 200
     else:
         return jsonify({"error": "Service not found"}), 404
+
 
 def get_all_services():
     services = Service.find_all()
